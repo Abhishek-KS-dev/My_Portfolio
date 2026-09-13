3a9dcfee8edb6dfdd76329d118e65183e3d35695
Abhishek K S <textwithak52@gmail.com>
2026-09-13 16:13:05 +0530

    patch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -282,8 +282,8 @@ export default function Navbar({
     style={{
       position: 'absolute',
       top: '60px',
-      left: '12px',
-      width: '220px',
+      left: '24px',
+      width: '240px',
       background: 'var(--bg-elevated)',
       border: '1px solid var(--border-color)',
       borderRadius: '12px',
